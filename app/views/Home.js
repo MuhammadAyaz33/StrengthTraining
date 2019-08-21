960d4513fb938961f97f31476df9b56261a2b09c
hamzaahmed742 <hamza.ahmed742@gmail.com>
2019-08-22 01:34:50 +0500

    add elapsed time and set time

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Header, Left, Body, Right, Title, Subtitle, Icon} from 'native-base';
 import { View ,Text,Image,TouchableOpacity} from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
-
+import moment from 'moment';
 export default class Home extends Component {
     static navigationOptions = () => ({
         title: 'Strength Training',
@@ -20,12 +19,95 @@ export default class Home extends Component {
     //     this, 'hideMsg', () => this.setState({showMsg: false}), 2000
     //   ));
     // }
+    constructor() {
+      super()
+      this.state = {setCount: 1, elapsedTime: "00:00", setTime: "00:00", Workout: true, workoutData: {setTime: []}}
+    }
+    nextSet = () => {
+      console.log(this.state.setTime, this.state.elapsedTime)
+      if(!this.state.Workout) {
+        this.setState((state) => {
+          return  {
+            setCount: state.setCount + 1, 
+            workoutData: {setCount: state.setCount, setTime: [...state.workoutData.setTime, state.setTime], elapsedTime: state.elapsedTime},
+            setTime: "00:00"
+          }
+        })
+        this.stopTimer(this.state.setTimeInterval)
+        // this.startSetTimer()
+        this.countDown()
+      }
+      console.log(this.state.workoutData)
+    }
+    startTimer = () => {
+      var startTimestamp = moment().startOf("day");
+      var interval = setInterval(() => { 
+        this.setState(() => {
+          return {elapsedTime: this.countUp(startTimestamp)}
+        })
+      }, 1000)
+      this.setState(() => {return {elapsedTimeInterval: interval}})
+    }
+
+    countDown = () => {
+      console.log("CAlled")
+      var time = 78;
+      var duration = moment.duration(time * 1000, 'milliseconds');
+
+      var interval = setInterval(() => {
+        duration = moment.duration(duration.asMilliseconds() - 1000, 'milliseconds');
+        console.log(moment(duration.asMilliseconds()).format('mm:ss'))
+        this.setState(() => {
+          return {setTime: moment(duration.asMilliseconds()).format('mm:ss')}
+        })
+      }, 1000)
+      this.setState(() => {return {setTimeInterval: interval}})
+
+    }
+    countUp = (startTimestamp) => {
+        startTimestamp.add(1, 'second');
+        return startTimestamp.format('mm:ss')
+    }
+    stopTimer = (interval) => {
+      clearInterval(interval)
+    }
+    toggleWorkout = () => {
+      this.setState((state) => {
+        return {Workout: !state.Workout, elapsedTime: "00:00", setTime: "00:00", setCount: 1,  workoutData: {setTime: []}}
+      })
+      if(!this.state.Workout){
+        this.stopTimer(this.state.elapsedTimeInterval)
+        this.stopTimer(this.state.setTimeInterval)
+        this.endWorkout()
+      } else {
+        this.startTimer()
+        this.countDown()
+      }
+    }
+
+    startSetTimer = () => {
+      var setTimeStamp = moment().startOf("day");
+      var interval = setInterval(() => { 
+        this.setState(() => {
+          return {setTime: this.countUp(setTimeStamp)}
+        })
+      }, 1000)
+      this.setState(() => {return {setTimeInterval: interval}})
+    }
+    endWorkout = () => {
+      this.setState((state) => {
+        return  {
+          workoutData: {setCount: state.setCount, setTime: [...state.workoutData.setTime, state.setTime], elapsedTime: state.elapsedTime},
+        }
+      })
+      console.log("TODO save workout to DB", this.state.workoutData)
+    }
   render() {
     const {
       containerStyle,
       circle,
       boltTxt,
-      textStyle,
+      txtStyle,
       makeCenter,
     } = myStyle
     return (
@@ -39,22 +121,22 @@ export default class Home extends Component {
             <Grid>
               <Row>
                 <Col style={[{height: 100},makeCenter ]}>
-                  <Text style={{textStyle}}>Elapsed</Text>
-                  <Text style={boltTxt}>19:23</Text>
+                  <Text style={txtStyle}>Elapsed</Text>
+                  <Text style={boltTxt}>{this.state.elapsedTime}</Text>
                 </Col>
                 <Text style={{borderRightWidth:1,borderColor:'blue',marginTop:10}}></Text>
                 <Col style={[{height: 140} ,makeCenter]}>
-                  <Text style={{textStyle}}>Set 7</Text>
-                  <Text style={boltTxt}>19:23</Text>
-                  <TouchableOpacity>
-                    <Text style={[textStyle,{marginTop:20}]}>Next Set</Text>
+                  <Text style={txtStyle}>Set {this.state.setCount}</Text>
+                  <Text style={boltTxt}>{this.state.setTime}</Text>
+                  <TouchableOpacity onPress={this.nextSet}>
+                    <Text style={[txtStyle,{marginTop:20}]}>Next Set</Text>
                   </TouchableOpacity>
                 </Col>
               </Row>
              
               <View style={makeCenter}>
-                <TouchableOpacity>
-                  <Text style={[textStyle,{marginTop:10,marginBottom:10}]}>End Workout</Text>
+                <TouchableOpacity onPress={this.toggleWorkout}>
+                  <Text style={[txtStyle,{marginTop:10,marginBottom:10}]}>{(this.state.Workout)? "Start" : "End"} Workout</Text>
                 </TouchableOpacity>
               </View>
             </Grid>
@@ -82,7 +164,7 @@ const myStyle = {
     fontSize:22,
     marginTop:10
   },
-  textStyle:{
+  txtStyle:{
     fontWeight:'100' ,
     fontSize:14
   },
